e6ac73efcc7986b70915321e0cfc0c5433be800e
monikasananse <monikasnns@gmail.com>
2023-05-17 17:10:05 +0530

    done

---
+++ signUp.js
@@ -0,0 +1,19 @@
+document.querySelector("#form").addEventListener("submit",submitFun);
+
+let arr=[];
+
+
+function submitFun(event){
+    event.preventDefault();
+    let name=document.querySelector("#name").value;
+    let phone=document.querySelector("#number").value ;
+
+    let obj={
+        name:name,
+        phone:phone
+    }
+    arr.push(obj);
+    console.log(arr);
+    localStorage.setItem("meeshoData",JSON.stringify(arr));
+
+}
